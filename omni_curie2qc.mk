0ff6a6e61aefa4aa08d4cde1c30698294f15431a
Kra1o5 <kra1o5x@gmail.com>
2014-06-05 15:07:18 +0200

    Update for omni

--- omni_curie2qc.mk
+++ omni_curie2qc.mk
@@ -1,9 +1,12 @@
+# Inherit some common Omni stuff
+$(call inherit-product, vendor/omni/config/common.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/bq/curie2qc/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := curie2qc
-PRODUCT_NAME := cm_curie2qc
+PRODUCT_NAME := omni_curie2qc
 PRODUCT_BRAND := bq
 PRODUCT_MANUFACTURER := bq
 PRODUCT_MODEL := bq Curie 2 Quad Core
